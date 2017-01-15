b03acd687af89e6dd9344af5de4b407257c4f2bf
Wanted19901 <wanted19901>
2017-01-15 16:11:58 +0300

    leha comit007

--- html/js/calc2.js
+++ html/js/calc2.js
@@ -1,4 +1,18 @@
-var cifra=7;
-var message;
-message=cifra;
-alert(cifra);
+
+
+var vhod = prompt('Кто пришел?', '');
+if (vhod !== 'Admin') {
+    alert('NoSukaa!')
+}
+ else {
+    var pass = prompt('EnterPassword');
+}
+    if (pass == 'Wanted') {
+        alert('Hello')
+    }
+ else{
+    alert('Nooooooo!')
+}
+
+
+
